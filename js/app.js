11c0c21f993feca20a83d3bb33dc4b4b6c701c82
Your Name <you@example.com>
2023-01-07 12:41:30 +0500

    git add

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let number = 10900
+let number1 = 750
+console.log(number * number1)
+let number2 = number * number1
+let number3 = 1227264
+let number4 = 120
+console.log(number3 * number4)
+let number5 = number3 * number4
+console.log(number2 + number5)
+let number6 = number2 + number5
+
+let alisher = +
+prompt('Sizda necha pul bor ?');
+
+if (number6 - alisher > 0) {
+    console.log('Alisher ozgina sabr qilish bular ekan !');
+}else {
+    console.log('Alisher oq yol !');
+}
+
+
+
+
